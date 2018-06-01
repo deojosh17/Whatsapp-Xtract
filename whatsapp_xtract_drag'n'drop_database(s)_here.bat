481d1f35d53699d0a9c8448ef9e98ba375a63f70
Rithvik Vibhu <rithvikvibhu@gmail.com>
2018-06-01 21:06:31 +0530

    Update to Whatsapp Xtract 2.3 (2018-04-25)

--- whatsapp_xtract_drag'n'drop_database(s)_here.bat
+++ whatsapp_xtract_drag'n'drop_database(s)_here.bat
@@ -3,13 +3,13 @@ set path=%~dp0;%pythonpath%;%path%
 if "%~nx1"=="" echo No input file given! You need to drag and drop one or more database file(s) onto this batch script!
 if "%~nx1"=="" goto end
 @ECHO ON
-python "%~dp0whatsapp_xtract.py" %1
+python "%~dp0whatsapp_xtract.py" %1 -w wa.db
 @ECHO OFF
 :getfile
 shift
 if "%~nx1"=="" goto end
 @ECHO ON
-python "%~dp0whatsapp_xtract.py" %1
+python "%~dp0whatsapp_xtract.py" %1 -w wa.db
 @ECHO OFF
 goto getfile
 :end
